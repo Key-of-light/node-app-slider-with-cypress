dcf7fce6a0d8464f3b54db3785f74f48bbb76bd8
Student Domena <student@Mac114R.local>
2024-04-04 19:11:04 +0200

    add stuff

--- cypress/e2e/slider.cy.js
+++ cypress/e2e/slider.cy.js
@@ -16,3 +16,49 @@ describe('Swiper Gallery Test', function () {
     cy.get('.swiper-slide-active').should('contain', 'Paris');
   });
 });
+
+describe('Swiper Gallery Test', function () {
+  it('Checks if can swipe in both directions', function () {
+    cy.visit('http://localhost:3000');
+    cy.get('.swiper-button-next').click();
+    cy.wait(2000);
+    cy.get('.swiper-slide-active').should('contain', 'United Kingdom');
+    cy.get('.swiper-button-prev').click();
+    cy.wait(2000);
+    cy.get('.swiper-slide-active').should('contain', 'Italy');
+  });
+});
+
+describe('Swiper Gallery Test', function () {
+  beforeEach(() => {
+    cy.viewport('iphone-5')
+  })
+  it('Checks if mobile works', function () {
+    cy.visit('http://localhost:3000');
+    cy.get('.swiper-button-next').click();
+    cy.wait(2000);
+    cy.get('.swiper-slide-active').should('contain', 'United Kingdom');
+    cy.get('.swiper-button-prev').click();
+    cy.wait(2000);
+    cy.get('.swiper-slide-active').should('contain', 'Italy');
+  });
+});
+
+describe('Swiper Gallery Test', function () {
+  it('Checks if displayed properly', function () {
+    cy.visit('http://localhost:3000');
+    cy.get('.wrapper').should('be.visible');
+    cy.get('.swiper-slide-active').should('be.visible');
+    cy.get('.swiper-slide-active').should('contain', 'Italy');
+    cy.get('.swiper-button-next').click();
+    cy.wait(2000);
+    cy.get('.swiper-slide-active').should('be.visible');
+    cy.get('.swiper-slide-active').should('contain', 'United Kingdom');
+    cy.get('.swiper-button-next').click();
+    cy.wait(2000);
+    cy.get('.swiper-slide-active').should('be.visible');
+    cy.get('.swiper-slide-active').should('contain', 'Paris');
+    cy.get('.swiper-button-next').should('not.be.disabled');
+    cy.get('.swiper-button-prev').should('not.be.disabled');
+  });
+});
